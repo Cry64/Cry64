f31180d14e268f60a8a7ec99988aab0eb9aa73ab
Cry64 <cry64.com@gmail.com>
2022-02-03 10:16:23 +1300

    BIG

--- JS/script.js
+++ JS/script.js
@@ -1,39 +1,19 @@
-.about-page {
-    border: 1px solid black;
-    margin: 20px;
-    height: 48.65vw;
-  }
-  
-  .about-container {
-    overflow: scroll;
-    height: 48.5vw;
-  }
-  
-  .video-background {
-  position: relative;
-  overflow: hidden;
-  width: 100vw;
-  height: 100vh;
-}
+<!--start of footer-->
 
-.video-background iframe {
-  position: absolute;
-  top: 50%;
-  left: 50%;
-  width: 100vw;
-  height: 100vh;
-  transform: translate(-50%, -50%);
-}
-
-@media (min-aspect-ratio: 16/9) {
-  .video-background iframe {
-    /* height = 100 * (9 / 16) = 56.25 */
-    height: 56.25vw;
-  }
-}
-@media (max-aspect-ratio: 16/9) {
-  .video-background iframe {
-    /* width = 100 / (9 / 16) = 177.777777 */
-    width: 177.78vh;
-  }
-}
+    <div class="footer-filler">
+      <div class="footer">
+        <a href="https://discord.gg/4NerM5KZtZ" target="_blank" class="footer-item discord">
+          <p>Discord</p>
+        </a>
+        <a href="https://www.youtube.com/channel/UCNgQmNYFrJ03TJUWSO0ekWw/featured" target="_blank" class="footer-item youtube">
+            <p>Youtube</p>
+        </a>
+        <a href="https://www.instagram.com/cry64_/" target="_blank" class="footer-item instagram">
+            <p>Instagram</p>
+        </a>
+        <a href="https://twitter.com/Cry64_" target="_blank"" class="footer-item twitter">
+            <p>Twitter</p>
+        </a>
+      </div>
+    </div>
+  </body>

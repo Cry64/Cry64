00fe796f96c06c0fb2a03d7555f4aa8103765155
Cry64 <96223045+Cry64@users.noreply.github.com>
2024-01-24 18:29:51 +1300

    Revert "new homepage test"
    This reverts commit 0f8f300747577a5756bde7d0e93cd1be81d95537.

---
+++ JS/script.js
@@ -0,0 +1,96 @@
+<!--start of footer-->
+
+    <div class="footer-filler">
+      <div class="footer">
+        <a href="https://discord.gg/4NerM5KZtZ" target="_blank" class="footer-item discord">
+          <p>Discord</p>
+        </a>
+        <a href="https://www.youtube.com/channel/UCNgQmNYFrJ03TJUWSO0ekWw/featured" target="_blank" class="footer-item youtube">
+            <p>Youtube</p>
+        </a>
+        <a href="https://www.instagram.com/cry64_/" target="_blank" class="footer-item instagram">
+            <p>Instagram</p>
+        </a>
+        <a href="https://twitter.com/Cry64_" target="_blank"" class="footer-item twitter">
+            <p>Twitter</p>
+        </a>
+      </div>
+    </div>
+  </body>
+
+        
+  <div class="card-container">
+  <a href="#" class="album">
+    <img class="album-cover" style="background-image:url(yourimage.jpg)"src="/IMAGES/dummy.png">
+    <div class="album-card">
+      <div class="album-title">
+        <p>Coming Soon</p>
+      </div>
+      <div class="album-artist">
+        <p></p>
+      </div>
+      <div class="genre-tags" id="genre-tagz">
+        <p>???</p>
+        <p class="tag second-tag">???</p>
+        <p class="tag third-tag">???</p>
+        <p class="fourth-tag" id="fourth-tag">???</p>
+      </div>
+    </div>
+  </a>
+  <a href="#" class="album">
+    <img class="album-cover" style="background-image:url(yourimage.jpg)"src="/IMAGES/dummy.png">
+    <div class="album-card">
+      <div class="album-title">
+        <p>Coming Soon</p>
+      </div>
+      <div class="album-artist">
+        <p></p>
+      </div>
+      <div class="genre-tags" id="genre-tagz">
+        <p>???</p>
+        <p class="tag second-tag">???</p>
+        <p class="tag third-tag">???</p>
+        <p class="fourth-tag" id="fourth-tag">???</p>
+      </div>
+    </div>
+  </a>
+  <a href="#" class="album">
+    <img class="album-cover" style="background-image:url(yourimage.jpg)"src="/IMAGES/dummy.png">
+    <div class="album-card">
+      <div class="album-title">
+        <p>Coming Soon</p>
+      </div>
+      <div class="album-artist">
+        <p></p>
+      </div>
+      <div class="genre-tags" id="genre-tagz">
+        <p>???</p>
+        <p class="tag second-tag">???</p>
+        <p class="tag third-tag">???</p>
+        <p class="fourth-tag" id="fourth-tag">???</p>
+      </div>
+    </div>
+  </a>
+  <a href="#" class="album">
+    <img class="album-cover" style="background-image:url(yourimage.jpg)"src="/IMAGES/dummy.png">
+    <div class="album-card">
+      <div class="album-title">
+        <p>Coming Soon</p>
+      </div>
+      <div class="album-artist">
+        <p></p>
+      </div>
+      <div class="genre-tags" id="genre-tagz">
+        <p>???</p>
+        <p class="tag second-tag">???</p>
+        <p class="tag third-tag">???</p>
+        <p class="fourth-tag" id="fourth-tag">???</p>
+      </div>
+    </div>
+  </a>
+</div>
+<!--start of view all releases-->
+
+<a href="../releases.html" class="view-entire-collection">
+  <p>View All Releases</p>
+</a>
